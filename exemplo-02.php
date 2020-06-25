2c65cc5eb9a925d95da4c797e091c4199faec211
patriciatamega <pat.tamega@gmail.com>
2020-06-25 15:43:30 -0300

    Aula 17 - operadores

--- exemplo-02.php
+++ exemplo-02.php
@@ -1,1 +1,17 @@
+<?php
 
+// Também um Operador de Atribuição:
+
+$valorTotal = 0;
+
+$valorTotal += 100;
+
+$valorTotal += 25;
+
+$valorTotal -= 10;
+
+$valorTotal *= .9;
+
+echo $valorTotal;
+
+?>
