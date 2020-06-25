0d0afda09fd174fc5ae66f237200a2770e1a1486
patriciatamega <pat.tamega@gmail.com>
2020-06-25 15:35:14 -0300

    Atualização após aula

--- exemplo-01.php
+++ exemplo-01.php
@@ -1,1 +1,22 @@
+<?php
 
+// Operadores de String:
+
+$nome = "Hcode";
+
+echo $nome . " mais alguma coisa<br>";
+
+/* variável: $nome
+   operador de atribuição: =
+   operador de concatenação: .
+   string: "Hcode" */
+
+
+
+// Operador Composto:
+
+$nome .= " Treinamentos";
+
+echo $nome;
+
+?>
