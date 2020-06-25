e52ddc872008157cf2974748fb238d7c89101646
patriciatamega <pat.tamega@gmail.com>
2020-06-25 16:01:31 -0300

    Aula 17 em operadores aritmétivos

--- exemplo-03.php
+++ exemplo-03.php
@@ -1,1 +1,30 @@
+<?php
 
+//Operadores Aritméticos:
+
+$a = 10;
+$b = 2;
+
+echo $a + $b;
+
+echo "<br>";
+
+echo $a - $b;
+
+echo "<br>";
+
+echo $a * $b;
+
+echo "<br>";
+
+echo $a / $b;
+
+echo "<br>";
+
+echo $a % $b; // módulo (que calcula se há restos da divisão)
+
+echo "<br>";
+
+echo $a ** $b; // 10 ao quadrado
+
+?>
