1e653829f5c32117b634f11ffc2724f2e41eaf7e
patriciatamega <pat.tamega@gmail.com>
2020-06-30 15:27:16 -0300

    Operador Spaceship

--- exemplo-05.php
+++ exemplo-05.php
@@ -1,1 +1,35 @@
+<?php
 
+$a = 50;
+
+$b = 35;
+
+// Operador Spaceship (comparação do tipo: maior, igual ou menor)
+
+var_dump($a <=> $b);
+
+echo "<br>";
+
+/////
+
+$c = 35;
+
+$d = 35;
+
+var_dump($c <=> $d);
+
+echo "<br>";
+
+/////
+
+$e = 50;
+
+$f = 60;
+
+var_dump($e <=> $f);
+
+echo "<br>";
+
+
+
+?>
