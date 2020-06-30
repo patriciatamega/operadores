c1997fc3aa02526773298852c74c787ed54e37a9
patriciatamega <pat.tamega@gmail.com>
2020-06-30 15:35:22 -0300

    Operador Null Colesce

--- exemplo-06.php
+++ exemplo-06.php
@@ -1,1 +1,24 @@
+<?php
 
+// Operador NULL Coalesce
+
+$a = NULL;
+
+$b = NULL;
+
+$c = 10;
+
+echo $a ?? $b ?? $c;
+
+echo "<br>";
+
+$d = NULL;
+
+$e = 8;
+
+$f = 10;
+
+echo $d ?? $e ?? $f;
+
+
+?>
