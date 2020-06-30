b088ba592885b5f8da6e0171183e3f25cf5783c2
patriciatamega <pat.tamega@gmail.com>
2020-06-30 15:56:41 -0300

    Parte final da aula 17

--- exemplo-08.php
+++ exemplo-08.php
@@ -1,1 +1,29 @@
+<?php
 
+$resultado = 10 + 3 / 2;
+
+echo $resultado;
+
+echo "<br>";
+
+$resultado2 = (10 + 3) / 2;
+
+echo $resultado2;
+
+echo "<br>";
+
+// && significa "e"
+
+$resultado3 = (10 + 3) / 2 > 5 && 10 + 5 < 20;
+
+var_dump ($resultado3);
+
+echo "<br>";
+
+// || significa "ou"
+
+$resultado4 = (10 + 3) / 2 > 5 || 10 + 5 < 20;
+
+var_dump ($resultado4);
+
+?>
